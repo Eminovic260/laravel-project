dc0315740e2c9802dfd95102d2b995f3033e0ba5
Emin Bahous <emin.bahous@le-campus-numerique.fr>
2025-07-22 15:20:11 +0200

    fin du 2.1 url route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get("/", function () {
+    return ('Homepage');
+});
+
+
+route::get('/product', function () {
+    return ("Liste des produits");
+});
+
+
+route::get('/product/{id}', function () {
+    return ("Fiche du produit {id}");
+});
+
+
+route::get('/cart', function () {
+    return ("Panier");
+});
